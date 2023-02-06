2abaa0aa77c5f2f485b445a676b5d596299cdaa1
Valter Aro <valteraro@gmail.com>
2023-02-06 14:16:44 +0200

    Initial commit

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueAxios from "vue-axios";
+import axios from "axios";
 
+Vue.use(VueAxios, axios);
 createApp(App).use(router).mount('#app')
